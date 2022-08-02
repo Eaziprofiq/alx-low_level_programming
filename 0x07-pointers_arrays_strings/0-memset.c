13f31802860177fef7272c308f59094eed701e18
Ezekiel Offiong <eaziprofiq07@gmail.com>
2022-08-02 10:58:28 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
-/* _memset - dive
+/** _memset - dive
  * @s: pointer to char params
  * @b: data to change
  * @n: index
  * return: *s
  */
 
-char *_memset(char*s, char b, unsigned int n) {
+char *_memset(char *s, char b, unsigned int n)
+{
 	unsigned int I;
 
 	for (i = 0; i < n; i++)
@@ -15,6 +16,6 @@ char *_memset(char*s, char b, unsigned int n) {
 		s[i] = b;
 	}
 
-	return(s);
+	return (s);
 
 }
