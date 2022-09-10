ea207de033d7f12a27dde5200ebdcf02822eb8a5
Ezekiel Offiong Peter <89645441+Eaziprofiq@users.noreply.github.com>
2022-09-10 14:47:48 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,16 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
 
-<<<<<<< HEAD
-	for (a = 0; );
-}
-=======
 	for (a = 0; a < n; a++)
 		dest[a] = src[a];
-<<<<<<< HEAD
 	return (dest);
 }
->>>>>>> 6c1eee895bad07010107fd0ee168dc94ca922f9e
-=======
-	return (dest);
->>>>>>> 77dcae431bfcf7f511cbe9bdc0ce78a13b0cb34f
